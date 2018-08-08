8619d0ea715ccfcd84ab1fad5bd45874d7b6d512
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-08 11:57:06 -0700

    ADD main function.

--- baby_dragon.py
+++ baby_dragon.py
@@ -28,4 +28,22 @@ class Dragon:
         self.direction = 'right'
 
 def main():
-    pass
+    pygame.init()
+    screen = pygame.display.set_mode((700, 700))
+    pygame.display.set_caption("Baby Dragon Display")
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((66, 244, 241))
+
+    if pygame.font:
+        font = pygame.font.Font(None, 36)
+        text = font.render("Look at the cute BABBY DRAGON!!!!!", 1, (0, 0, 200))
+        textpos = text.get_rect(centerx = background.get_width()/2, centery = background.get_height()/2)
+
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    clock = pygame.time.Clock()
+    dragon = Dragon()
+    sprite = pygame.sprite.RenderPlain(dragon)
