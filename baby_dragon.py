ff850d66f79315fa729020ef791b2e6c3f58e478
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-08 12:21:39 -0700

    ADD load image function.

--- baby_dragon.py
+++ baby_dragon.py
@@ -2,21 +2,25 @@
 # -*- coding: utf-8 -*-
 import os, pygame
 from pygame.locals import *
+
+def load_image(name, colorkey=None):
+    try:
+        image = pygame.image.load(name)
+    except pygame.error:
+        print("Whoops! I'm unable to load: ", name, " sorry about that
+        
 # Parent Class
-class Dragon:
-    def __init__(self, Sprite, name, snuggled,
-     sleep, hunger, cloaca,
-      fire, energy, maturity
-      ):
-        Sprite.__init__(self)
-        self.name = name
-        self.snuggled = snuggled
-        self.sleep = sleep
-        self.hunger = hunger
-        self.cloaca = cloaca
-        self.fire = fire
-        self.energy = energy
-        self.maturity = maturity
+class Dragon(pygame.sprite.Sprite):
+    def __init__(self):
+        # self.name = name
+        # self.snuggled = snuggled
+        # self.sleep = sleep
+        # self.hunger = hunger
+        # self.cloaca = cloaca
+        # self.fire = fire
+        # self.energy = energy
+        # self.maturity = maturity
+        # pygame instance attributes.
         self.image, self.rect = load_image('baby_dragon_by_kaitlynclinkscales-d55vsza.png', -1)
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
