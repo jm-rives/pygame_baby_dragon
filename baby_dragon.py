71d10186475e2f44371f145c13a2cd1473adaacd
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-08 11:37:01 -0700

    ADD Sprite inheritance and class attributes.

--- baby_dragon.py
+++ baby_dragon.py
@@ -4,7 +4,11 @@ import os, pygame
 from pygame.locals import *
 # Parent Class
 class Dragon:
-    def __init__(self, name, snuggled, sleep, hunger, cloaca, fire, energy, maturity):
+    def __init__(self, Sprite, name, snuggled,
+     sleep, hunger, cloaca,
+      fire, energy, maturity
+      ):
+        Sprite.__init__(self)
         self.name = name
         self.snuggled = snuggled
         self.sleep = sleep
@@ -13,3 +17,12 @@ class Dragon:
         self.fire = fire
         self.energy = energy
         self.maturity = maturity
+        self.image, self.rect = load_image('baby_dragon_by_kaitlynclinkscales-d55vsza.png', -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.STEP = 9
+        self.MARGIN = 12
+        self.xstep = self.STEP
+        self.ystep = 0
+        self.degrees = 0
+        self.direction = 'right'
