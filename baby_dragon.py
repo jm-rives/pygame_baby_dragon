7f615877d34c6b238968e63776da6e617e12d81b
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-08 11:18:39 -0700

    ADD basic pygame libraries.

--- baby_dragon.py
+++ baby_dragon.py
@@ -1,6 +1,7 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
 # -*- coding: utf-8 -*-
-
+import os, pygame
+from pygame.locals import *
 # Parent Class
 class Dragon:
     def __init__(self, name, snuggled, sleep, hunger, cloaca, fire, energy, maturity):
